483f363a7c5b71dca4969c58cbe333a328a5393a
gerrybezhani <gerrybezhani@gmail.com>
2017-05-07 12:44:02 +0300

    finished with cve stix generator(still some bugs to squash)

--- src/main/java/RomeLibraryExample.java
+++ src/main/java/RomeLibraryExample.java
@@ -12,7 +12,7 @@ public class RomeLibraryExample {
     public static void main(String[] args){
 
         //test call to test C2 IP stix producer
-       //StixProducer.produce("192.168.1.1","Harvester");
+       StixProducer.produce("192.168.1.1","Harvester");
 
 
 
