d022840894957fdd3884f3d67f3ad227ab88c46e
gerry <gerrybezhani@gmail.com>
2017-05-07 18:59:46 +0300

    added CVE produce implemetetion(in progress)

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,4 +1,13 @@
+import com.threatconnect.sdk.client.reader.AbstractGroupReaderAdapter;
+import com.threatconnect.sdk.client.reader.ReaderAdapterFactory;
+import com.threatconnect.sdk.config.Configuration;
+import com.threatconnect.sdk.conn.Connection;
+import com.threatconnect.sdk.exception.FailedResponseException;
+import com.threatconnect.sdk.server.entity.Adversary;
+
+import java.io.IOException;
 import java.util.Calendar;
+import java.util.List;
 
 /**
  * Created by gerry on 4/23/2017.
@@ -6,7 +15,27 @@ import java.util.Calendar;
 public class Test {
     public static void main(String[] args) {
 
-        Calendar cal = HelperMethods.getDateFromString("06 Mar 2017");
-        System.out.println(cal.getTime());
+        //Calendar cal = HelperMethods.getDateFromString("06 Mar 2017");
+        //System.out.println(cal.getTime());
+
+        //Testiong ThreatCOnnect SDK
+        Connection conn = null;
+
+        try {
+                        Configuration configuration = new Configuration("ttps://api.threatconnect.com/v2/","","","gerrybezhani");
+                         conn = new Connection();
+
+                        AbstractGroupReaderAdapter<Adversary> reader = ReaderAdapterFactory.createAdversaryGroupReader(conn);
+                         List<Adversary> data = (List<Adversary>) reader.getAll("System");
+                         for (Adversary g : data ) {
+                             System.out.println( "Adversary: " + g.toString() );
+                        }
+
+                     } catch (IOException ex ) {
+                         System.err.println("Error: " + ex);
+                     } finally {
+                         if ( conn != null )     conn.disconnect();
+                     }
+
     }
 }
